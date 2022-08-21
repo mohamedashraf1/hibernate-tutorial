e70e88f49f2e654cadcabc5346cff78573b0ccf3
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-08-21 23:55:12 +0200

    defined the id generation strategy

--- src/hibernate/demo/entity/Student.java
+++ src/hibernate/demo/entity/Student.java
@@ -2,6 +2,8 @@ package hibernate.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,8 +11,9 @@ import javax.persistence.Table;
 @Table(name = "student")
 public class Student {
 	
-	
+	// this strategy means to let the database decide the id value
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 	
